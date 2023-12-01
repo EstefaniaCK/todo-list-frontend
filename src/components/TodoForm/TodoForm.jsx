b14f7c8866254d7c39e2f29e20f946ae00f25f35
Estefck <estef.komu@gmail.com>
2023-11-30 20:10:38 -0500

    OnSubmit handler and Onchange handler have been added to the form

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = (props) => {
     const [input, setInput] = useState('')
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.random(),
+            text: input
+        })
+
+        setInput('');
+    };
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
     return (
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
             <input
                 className='form__input'
                 type="text"
                 placeholder='Add your task'
                 value={input}
                 name="text"
+                onChange={handleChange}
             />
-            <button className='form__button'>Add</button>
+            <button className='form__button' type="submit">Add</button>
         </form>
     );
 };
