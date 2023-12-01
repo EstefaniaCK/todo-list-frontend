23ca48a8da5461403ca4637480e8745d91579b96
Estefck <estef.komu@gmail.com>
2023-11-30 20:48:13 -0500

    functionality for setting new todos was added

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,12 +3,20 @@ import TodoForm from '../TodoForm/TodoForm';
 
 const TodoList = () => {
 
-    const [tasks, setTasks] = useState([]);
+    const [todos, setTodos] = useState([]);
+    const addTodo = (todo) => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+    }
 
     return (
         <div>
             <h1>Today's Tasks</h1>
-            <TodoForm />
+            <TodoForm onSubmit={addTodo} />
         </div>
     );
 };
